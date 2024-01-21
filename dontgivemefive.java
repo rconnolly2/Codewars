70cd8b4c07db2a148c5e9c775653b30290725647
Robert Connolly <robertoconnolly100@gmail.com>
2024-01-21 01:56:15 +0100

    dontgivemefive

--- dontgivemefive.java
+++ dontgivemefive.java
@@ -1,12 +1,17 @@
 public class dontgivemefive {
-    public static int dontGiveMeFive(int start, int end)
-    {
-      int r = 0;
-      for (int j=start; j<=end; j++) {
-        r += (j==5 || j<0 || j%10==5) ? 0 : 1;
-      }
-      return r;
+      public static int dontGiveMeFive(int start, int end) {
+        int contador = 0;
+        
+        for (int i=start; i<=end; i++) { // itero desde el principio hasta el final
+            if (String.valueOf(i).contains("5")) { // si es 5 salto el ciclo
+                continue;
+            }
+            contador++; // aumento contador
+        }
+        
+        return contador;
     }
+
     public static void main(String[] args) {
         System.out.println(dontGiveMeFive(4, 17));
     }
