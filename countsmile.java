23441bc7a37e6b1615986294d3a21121cc5184b1
Robert Connolly <robertoconnolly100@gmail.com>
2024-02-14 00:08:40 +0100

    countsmile

--- countsmile.java
+++ countsmile.java
@@ -1,14 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class countsmile {
-      public static int countSmileys(List<String> arr) {
-        Integer contador = 0;
+    public static int countSmileys(List<String> arr) {
+      Integer contador = 0;
       // Just Smile :)
       for (String str : arr) {
         if ((str.charAt(str.length()-1)==')' || str.charAt(str.length()-1)=='D') && (str.charAt(0)==':' || str.charAt(0)==';') && str.length()==2){
             contador++;
-        } else if (((str.charAt(str.length()-1)==')' || str.charAt(str.length()-1)=='D') && (str.charAt(0)==':' || str.charAt(0)==';') && () && str.length()==3))
+        } else if ((str.charAt(str.length()-1)==')' || str.charAt(str.length()-1)=='D') && (str.charAt(0)==':' || str.charAt(0)==';') && (str.charAt(1)=='~' || str.charAt(1)=='-') && str.length()==3) {
+            contador++;
+        }
       }
       return contador;
+    }
+
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>(Arrays.asList(":)", ":D", ":-}", ":-()"));
+        System.out.println(countSmileys(lista));
     }
 }
